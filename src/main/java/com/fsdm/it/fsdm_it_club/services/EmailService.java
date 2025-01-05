4ac6895d866f2c2d4954f34abaa9d716a09f7e41
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-01-05 03:32:34 +0100

    Update copyright year to include 2025 across services and configurations

--- src/main/java/com/fsdm/it/fsdm_it_club/services/EmailService.java
+++ src/main/java/com/fsdm/it/fsdm_it_club/services/EmailService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 FSDM IT Club.
+ * Copyright (c) 2024, 2025 FSDM IT Club.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
