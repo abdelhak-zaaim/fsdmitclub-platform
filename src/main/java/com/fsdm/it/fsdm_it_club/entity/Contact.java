310d4686ee439d6cf735ec875191bfb5b811bdf5
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-01-11 11:51:08 +0100

    coe refactor

--- src/main/java/com/fsdm/it/fsdm_it_club/entity/Contact.java
+++ src/main/java/com/fsdm/it/fsdm_it_club/entity/Contact.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 FSDM IT Club.
+ * Copyright (c) 2024, 2025 FSDM IT Club.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 
 package com.fsdm.it.fsdm_it_club.entity;
 
